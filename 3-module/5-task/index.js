b3464000ea1e527f53373165c7843a6ca027f394
Vitalii Mikhalevskii <vitalymihale@yandex.ru>
2021-07-02 17:56:41 +0300

    Исправления

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,5 +1,3 @@
-str = '1, -5.8 или 10, хотя 34 + -5.3 и 73'
-
 function getMinMax(str) {
   const array = str
       .split(',')
@@ -10,5 +8,3 @@ function getMinMax(str) {
       .sort((a, b) => a - b)
   return {min: array[0], max: array[array.length - 1] }
 }
-
-console.log(getMinMax(str))
